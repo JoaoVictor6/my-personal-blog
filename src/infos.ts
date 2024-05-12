1c82bc18c0f0c9d9b66e27698fe1d4e2fda557cf
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2024-05-12 01:31:21 -0300

    feat: add certificate area

---
+++ src/infos.ts
@@ -0,0 +1,29 @@
+
+export const workExperience = [
+	{
+		company: 'One Monitoramento',
+		timeRange: '05/2023 - 10/2023',
+		description: 'Trabalhei em uma plataforma de monitoramento de veículos, onde o projeto era desenvolvido em React e Express. Ao ingressar no projeto, concentrei meus esforços na melhoria de desempenho. Implementei caches pontuais em consultas recorrentes e redesenhei toda a interface da plataforma. Além disso, dediquei-me à otimização de performance, especialmente em áreas relacionadas a mapas e rastreamento no frontend.',
+		topics: [
+			'Manutenção de plataforma feita em React',
+			'Melhoria de performance',
+			'Otimização de consultas na API',
+			'Mudança completa no design system da plataforma'
+		]
+	},
+	{
+		company: 'CAIO HENRIQUE DE OLIVEIRA BORGHI PROGRAMAS E INFORMATICA',
+		timeRange: '01/2023 - 05/2024',
+		description: `Trabalhei na criação de SaaS, projetos open source e extensões para o Visual Studio Code. Um dos casos mais desafiadores foi a criação do Trampar de Casa (https://trampardecasa.com.br/), um projeto open source focado em indexar vagas home office, tanto nacionais quanto globais, para desenvolvedores brasileiros. Desenvolvi uma infraestrutura na AWS com lambdas, EC2 e SNS. Conseguimos estabelecer uma plataforma barata e performática que atende mais de 19 mil desenvolvedores no Brasil.
+Além disso, também desenvolvi um SaaS em React (Next.js) focado na geração de conteúdo e copywriting com inteligência artificial. Com o tempo, expandimos esse mesmo produto para extensões de navegadores, essencialmente uma versão simplificada dos recursos da plataforma.
+Também desenvolvemos outro serviço com inteligência artificial, uma extensão para o editor de textos chamado Visual Studio Code. Nela, integramos o VSCode com a OpenAI.
+`,
+		topics: [
+			'Criação de produtos digitais (SaaS)',
+			'Gerenciar diversos produtos',
+			'Estratégias de conversão e SEO(GA4, Google Search Console)',
+			'Criação e manutenção de infraestruturas no ecosistema AWS',
+			'Criação de extensão para o google chrome'
+		]
+	}
+]
